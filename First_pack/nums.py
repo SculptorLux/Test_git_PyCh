afffa8f3b4a5af54eac7eda86d97da91e0b5e796
SculptorLux <alexanisin@gmail.com>
2023-06-08 12:10:46 +0300

    Добавлен модуль Up Удалена переменная text из nums

--- First_pack/nums.py
+++ First_pack/nums.py
@@ -2,13 +2,14 @@ def plus(a, b):
     return a + b
 
 
-text = "new text"
-
-
 def minus(a, b):
     return a - b
 
 
+def mul(h, r):
+    return h * r
+
 if '__main__' == __name__:
     print(plus(2, 2))
     print(minus(5, 2))
+    print(mul(2, 4))
